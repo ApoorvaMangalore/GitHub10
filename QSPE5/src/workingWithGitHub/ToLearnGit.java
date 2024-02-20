a3226a578b65124dfaf90a75f925cab11c51361f
Apoorva <Apoorva@TYSS-Apoorva>
2024-02-20 19:20:00 +0530

    3rd update.

--- QSPE5/src/workingWithGitHub/ToLearnGit.java
+++ QSPE5/src/workingWithGitHub/ToLearnGit.java
@@ -1,7 +1,7 @@
 package workingWithGitHub;
 
 public class ToLearnGit {
-public static void main(String[] args) {
-	System.out.println("hello");
-}
+	public static void main(String[] args) {
+		System.out.println("hello");
+	}
 }
